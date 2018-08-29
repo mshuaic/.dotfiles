ed957ce45d24f7c1acc5b432790ac181f21a56e5
Mark <mshuaic@hotmail.com>
2018-08-28 21:37:22 -0400

    08/28/2018

--- init.sh
+++ init.sh
@@ -3,14 +3,22 @@ sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
      libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
      xz-utils tk-dev libffi-dev
 
-sudo apt install -y emacs25
-sudo apt install -y xclip
+sudo apt install -y xclip libevent-dev texinfo build-essential texinfo libx11-dev \
+	 libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev \
+	libncurses-dev libxpm-dev automake autoconf libgnutls-dev mailutils
+
+
+# sudo apt install -y emacs25
+# sudo apt install -y xclip
 
 # pyenv
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 # echo 'export PATH="~/.pyenv/bin:$PATH"' >> ~/.bashrc
 # echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 # echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+pip install autopep8
+pip install virtualenv
 
 # docker 
 curl -fsSL get.docker.com -o get-docker.sh
