a2fb3f87f67240ecf6433040c30ace0966e3c4a8
Shuaicheng Ma <mshuaic@users.noreply.github.com>
2019-05-01 18:53:53 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,13 +9,12 @@ sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 
 # pyenv
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-# echo 'export PATH="~/.pyenv/bin:$PATH"' >> ~/.bashrc
-# echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-# echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+export PATH="~/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 cp .bashrc .emacs .tmux.conf .inputrc ~
-source ~/.bashrc
-hash -r
+# source ~/.bashrc
 
 # docker 
 # curl -fsSL get.docker.com -o get-docker.sh
