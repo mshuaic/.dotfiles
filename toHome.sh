609d1b4a2d39e66f9f64595addc87fb3690660ff
Mark <mshuaic@users.noreply.github.com>
2019-09-18 22:15:57 -0400

    zsh

--- toHome.sh
+++ toHome.sh
@@ -1,1 +1,14 @@
-cp .bashrc .emacs .tmux.conf .inputrc .myshell.sh $HOME
+#!/bin/bash
+
+# cp .bashrc .emacs .tmux.conf .inputrc .myshell.sh $HOME
+# make symbolic links for all dot files
+
+dotfiles=( .bashrc .emacs .inputrc .myshell.sh .zshrc )
+
+for i in "${dotfiles[@]}"
+do
+    ln -s -f $HOME/$i ./$i
+done
+
+ln -s -f $HOME/.tmux.conf ./.tmux/.tmux.conf
+
