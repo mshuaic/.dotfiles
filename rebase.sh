3b3a5fd2709842303b2de25446dfc88b7d3b919c
Mark <mshuaic@users.noreply.github.com>
2019-09-21 20:38:02 -0400

    updated

--- rebase.sh
+++ rebase.sh
@@ -1,4 +1,4 @@
-git config --global user.email "526473+gb96@users.noreply.github.com"
+git config --global user.email "mshuaic@users.noreply.github.com"
 git rebase -i
 git commit --amend --reset-author
 git rebase --continue
