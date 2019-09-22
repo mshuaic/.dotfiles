47350ef28857ac3ed506fab7824f55e7b1c8187e
Mark <mshuaic@users.noreply.github.com>
2019-09-21 22:58:52 -0400

    updated

--- toHome.sh
+++ toHome.sh
@@ -3,7 +3,7 @@
 # cp .bashrc .emacs .tmux.conf .inputrc .myshell.sh $HOME
 # make symbolic links for all dot files
 
-dotfiles=( .bashrc .emacs .inputrc .myshell.sh .zshrc )
+dotfiles=( .bashrc .emacs .inputrc .myshell.sh .zshrc .tmux.conf.local )
 currpath=`pwd`
 #echo $currpath
 
