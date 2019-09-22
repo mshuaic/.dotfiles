1d597957a9555b6a23e9dec364580905c57f1bd1
Mark <mshuaic@users.noreply.github.com>
2019-09-21 20:37:38 -0400

    updated

--- toHome.sh
+++ toHome.sh
@@ -4,11 +4,14 @@
 # make symbolic links for all dot files
 
 dotfiles=( .bashrc .emacs .inputrc .myshell.sh .zshrc )
+currpath=`pwd`
+#echo $currpath
 
 for i in "${dotfiles[@]}"
 do
-    ln -s -f $HOME/$i ./$i
+    ln -s -f $currpath/$i $HOME
 done
 
-ln -s -f $HOME/.tmux.conf ./.tmux/.tmux.conf
-
+ln -s -f $currpath/.tmux $HOME
+ln -s -f $currpath/.oh-my-zsh $HOME
+ln -s -f $currpath/.tmux/.tmux.conf $HOME
