e013a2d9d026baad5077673179f3bcab13f091ab
Mark <mshuaic@users.noreply.github.com>
2019-12-26 22:31:32 -0500

    updated

--- .myshell.sh
+++ .myshell.sh
@@ -92,7 +92,7 @@ agent_load_env
 # agent_run_state: 0=agent running w/ key; 1=agent w/o key; 2= agent not running
 agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
 
-if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
+if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ] || [ ! "$SSH_AGENT_PID" ] ; then
     agent_start
     # ssh-add
 elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
