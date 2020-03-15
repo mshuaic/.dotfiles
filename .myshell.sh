210107c00320681d0660bf9427cb21822d74dc54
Mark <mshuaic@users.noreply.github.com>
2020-03-14 20:23:11 -0400

    updated

--- .myshell.sh
+++ .myshell.sh
@@ -102,30 +102,3 @@ fi
 # local bin and library
 export PATH=$HOME/.local/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/.local/lib:$LD_LIBRARY_PATH
-
-
-
-# ssh agent forwarding
-# env=~/.ssh/agent.env
-
-# agent_load_env () { test -f "$env" && . "$env" >| /dev/null ; }
-
-# agent_start () {
-#     (umask 077; ssh-agent >| "$env")
-#     . "$env" >| /dev/null ; }
-
-# agent_load_env
-
-# # agent_run_state: 0=agent running w/ key; 1=agent w/o key; 2= agent not running
-# agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
-
-# if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
-#     agent_start
-#     # ssh-add
-# elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
-#     ssh-add
-# fi
-
-# unset env
-
-# trap 'test -n "$SSH_AUTH_SOCK" && eval `/usr/bin/ssh-agent -k`' 0
